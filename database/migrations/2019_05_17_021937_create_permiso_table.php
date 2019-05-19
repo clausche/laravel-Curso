872823cf10ea2042216bf334aae5a76e8641a4f5
Claudio Scheuermann <clausche@gmail.com>
2019-05-18 21:47:56 -0400

    adminlte

--- database/migrations/2019_05_17_021937_create_permiso_table.php
+++ database/migrations/2019_05_17_021937_create_permiso_table.php
@@ -15,8 +15,8 @@ class CreatePermisoTable extends Migration
     {
         Schema::create('permiso', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('nombre',50);
-            $table->string('slug',50);
+            $table->string('nombre',50)->unique();
+            $table->string('slug',50)->unique();
             $table->timestamps();
         });
     }
