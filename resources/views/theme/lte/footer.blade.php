a6d368a4059429d689c4d6b7fe51aeeb7d9b54bb
Claudio Scheuermann <clausche@gmail.com>
2019-05-19 20:41:14 -0400

    blade admin lte

---
+++ resources/views/theme/lte/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 0.1
+    </div>
+    <strong>@clausche &copy; 2019-2021 <a href="https://adminlte.io">Claudio Sch</a>.</strong> All rights
+    reserved.
+</footer>
